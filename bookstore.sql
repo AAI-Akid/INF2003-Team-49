48a0b491ae64e0eedcf7b55d9af5ceb3774767cf
AAI-Akid <2302868@sit.singaporetech.edu.sg>
2024-10-11 17:37:43 +0800

    adding user table sql

--- bookstore.sql
+++ bookstore.sql
@@ -1,21 +1,15 @@
-import pandas as pd
 DROP DATABASE IF EXISTS bookstore;
 
 CREATE DATABASE bookstore;
 
 USE bookstore;
 
-CREATE TABLE books (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    author VARCHAR(255) NOT NULL,
-    price DECIMAL(10, 2) NOT NULL
-);
-
-INSERT INTO books (title, author, price) VALUES
-('Book One', 'Author A', 9.99),
-('Book Two', 'Author B', 12.99),
-('Book Three', 'Author C', 15.99)
-('Book Four', 'Author D', 18.99);
-
-
+CREATE TABLE `bookstore`.`user` (
+  `user_id` INT NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(50) NOT NULL,
+  `email` VARCHAR(30) NOT NULL,
+  `password` CHAR(72) NOT NULL,
+  `address` TEXT NOT NULL,
+  `role` ENUM('user', 'admin') NULL DEFAULT 'user',
+  PRIMARY KEY (`user_id`),
+  UNIQUE INDEX `user_id_UNIQUE` (`user_id` ASC) VISIBLE);
